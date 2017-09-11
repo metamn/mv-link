590730e4d0e9607770969cc081ee262b4bcf94cf
metamn <bartus.csongor@gmail.com>
2017-09-11 17:29:36 +0300

    Making a Vue component

---
+++ link.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script>
+  export default  {
+    name: 'link',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .link {
+
+  }
+</style>
